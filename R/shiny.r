957a96288fdf48dc68cf559b4a0fe0cd2e5d60cb
Zeyad Abdelmottaleb <zabdelmottaleb@gmail.com>
2016-07-01 15:47:33 -0500

    Update shiny.r

--- R/shiny.r
+++ R/shiny.r
@@ -19,3 +19,15 @@ renderStreamgraph <- function(expr, env = parent.frame(), quoted = FALSE) {
   if (!quoted) { expr <- substitute(expr) } # force quoted
   shinyRenderWidget(expr, streamgraphOutput, env, quoted = TRUE)
 }
+
+
+event_data <- function(event = c("d3_hover", "d3_click", "d3_selected", 
+                                 "d3_relayout"), source = "A") {
+  session <- shiny::getDefaultReactiveDomain()
+  if (is.null(session)) {
+    stop("No reactive domain detected. This function can only be called \n",
+         "from within a reactive shiny context.")
+  }
+  val <- session$input[[sprintf(".clientValue-%s-%s", event[1], source)]]
+  if (is.null(val)) val else jsonlite::fromJSON(val)
+}
